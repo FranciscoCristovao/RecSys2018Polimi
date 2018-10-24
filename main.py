70c5cf40e677c2505fcd5d725a695fb587e389b1
FranciscoCristovao <francisco.m.cristovao@gmail.com>
2018-10-24 15:22:28 +0200

    Collaborative fast fixed, suggesting Top Songs

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from cbfRS.cbfRS import CbfRS
 from loader.loader import save_dataframe, train_data, target_data, full_data, test_data, tracks_data
 from utils.auxUtils import Evaluator
@@ -15,20 +14,17 @@ save_dataframe('output/submission_top_pop.csv', ',', predictions)
 '''
 
 #CBF
-rs = CbfRS(tracks_data, 10)
+# rs = CbfRS(tracks_data, 10)
 
 # Collaborative Filter User - User
-# rs = ColBfUURS()
+rs = ColBfUURS(10)
 rs.fit(train_data)
 
-
 predictions = rs.recommend(target_data['playlist_id'])
-pred_slow = rs.recommend_slower(target_data['playlist_id'])
+
 evaluator = Evaluator()
-
 evaluator.evaluate(predictions, test_data)
-evaluator.evaluate(pred_slow, test_data)
 
-save_dataframe('output/content.csv', ',', predictions)
-save_dataframe('output/content_slow.csv', ',', pred_slow)
+save_dataframe('output/output_content.csv', ',', predictions)
+
 
