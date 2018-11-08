49238c7e5e0896b2e2ce65a0e5011abb74d3774c
AlessandroArtoni <alessandro1.artoni@mail.polimi.it>
2018-11-08 13:54:57 +0100

    Added main to run SLIM_BPR

--- main_test.py
+++ main_test.py
@@ -1,13 +1,13 @@
-from hybrid_col_cbf_RS.hybrid_p_sampling import HybridRS
 from loader.loader import save_dataframe, train_data, target_data, full_data, test_data, tracks_data
 from utils.auxUtils import Evaluator
+from slimRS.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 
-#Hybrid (cbf - colf)
-rs = HybridRS(tracks_data, 10)
+# SLIM_Cython
+rs = SLIM_BPR_Cython(train_data)
+rs.fit()
+prediction = rs.recommend(target_data['playlist_id'])
+
 evaluator = Evaluator()
-rs.fit(train_data)
-max_res = rs.recommend(target_data['playlist_id'], 3, 6, 2)
-temp = evaluator.evaluate(max_res, test_data)
+evaluator.evaluate(prediction, test_data)
 
-# print(df)
-save_dataframe('output/hybrid_i_i_u_u_3_6_2.csv', ',', max_res)
+save_dataframe('output/slim_bpr_max2.csv', ',', prediction)
